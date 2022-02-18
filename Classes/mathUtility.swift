726c45063ca1746c885e7d5308ba02c39769da26
amanK1n <amankonen0799@gmail.com>
2022-02-18 10:18:55 +0530

    new class added

---
+++ Classes/mathUtility.swift
@@ -0,0 +1,25 @@
+//
+//  mathUtility.swift
+//  myMathUtility
+//
+//  Created by comviva on 18/02/22.
+//
+
+
+import Foundation
+public class mathUtility {
+    public init() {}
+   public func add(a: Int, b: Int) -> Int{
+        return a + b
+    }
+    
+   public func multiply(a: Int, b: Int) -> Int{
+        return a * b
+    }
+    
+    func divide(a: Int, b: Int) -> Int{
+        return a / b
+    }
+    
+    
+}
--- Example/myMathUtility/ViewController.swift
+++ Example/myMathUtility/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import myMathUtility
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let m = mathUtility()
+        var r1 = m.add(a: 11, b: 22)
+        r1 = m.multiply(a: 23, b: 11)
     }
 
     override func didReceiveMemoryWarning() {
